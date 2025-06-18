29cbb2996c6c171ef1654c78d254dbec783b77e6
adeshingale3 <adeshingale600@gmail.com>
2025-06-18 15:38:10 +0530

    Color Changing Button Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
+import '@fontsource/poppins'; 
+import RotationCard from './components/Card/RotationCard';
+import ButtonColorChange from './components/Buttons/ButtonColorChange';
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
+      <ButtonColorChange />
+      {/* <RotationCard /> */}
     </div>
   )
 }
---
+++ src/components/Buttons/ButtonColorChange.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ButtonColorChange = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+  <button className="
+  bg-gradient-to-r from-white to-black text-white 
+  bg-[length:400%_auto] bg-right border border-white 
+  hover:bg-left transition-all duration-1200 
+  hover:text-black px-6 py-3 rounded-3xl hover:border-black
+">
+  Hover me
+</button>
+
+    </div>
+  )
+}
+
+export default ButtonColorChange
