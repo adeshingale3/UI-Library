acd9d45fbd4565d68b7d8002f276584248589b69
adeshingale3 <adeshingale600@gmail.com>
2025-07-18 16:19:34 +0530

    modification in teammembercard

--- src/components/Card/TeamMemberCard.jsx
+++ src/components/Card/TeamMemberCard.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 
 const TeamMemberCard = () => {
     return (
-        <div className='h-80 w-60 rounded-4xl shadow-lg m-10 overflow-hidden z-10 relative'>
+        <div className='h-80 w-60 rounded-4xl shadow-lg m-10 overflow-hidden z-10 relative hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer group'>
             <div className='flex h-full w-full z-20 absolute mask-b-from-20% mask-b-to-98%'>
                 <img src='/images/Adesh.jpeg' className='object-cover h-full w-full brightness-110' />
             </div>
             <div className="absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-t from-white/20 to-transparent z-30 flex items-end px-4 pb-2">
-                <div className="flex justify-between items-center w-full mb-4">
+                <div className="flex justify-between items-center w-full mb-2">
                     
                     <div className="flex flex-col">
                         <h2 className="text-white text-[12px] font-semibold">Adesh Ingale</h2>
